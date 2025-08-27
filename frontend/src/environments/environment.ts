a2af22f2bb7a2d1d66e72ee33839e5ec300278c7
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-08-27 22:30:31 +0530

    upd

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   // Switch between local and deployed backend
   //apiUrl: 'http://localhost:8080/api', // Local backend
-   apiUrl: 'https://email-signature-generator-back.onrender.com/api', // Deployed backend
+   apiUrl: 'https://email-signature-generator-back.onrender.com', // Deployed backend
   stripePublishableKey: 'pk_test_51RLLXSGbDs9DLdlkYourPublishableKeyHere'
 };
